59e9f9826510c08d91cad82ec697939afa4b93ea
Divya Swaroopini Shridhar <swaroopini13@gmail.com>
2021-08-01 11:27:55 +0530

    Made a lot of changes, check updated documentation between the code and at the end of this file

--- SystemDesign.cpp
+++ SystemDesign.cpp
@@ -2,18 +2,33 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <stdlib.h>
+#include <experimental/filesystem>
+#include <fstream>
 
+namespace filesys = std::experimental::filesystem;
  
 using namespace std;
 /* _________________________________________________________________________DIRECTORY TREE____________________________________________________________________________*/
-
-class DirectoryTree : File {
+//Search Directory function yet to be implemented
+class DirectoryTree {
     public:
-        //files in the directory
-        vector<vector<string>> files;
+        //Multiple instances of this structure are created to store file details.
+        //each instance can be accessed via a vector and an index is used to trace the files
+        struct FileData{
+            int size;
+            time_t timestamp;
+            string filename;
+            string type;
+            File *file;
+        };
+        int total_no_of_files = 0;
+        vector<FileData> new_file;
+
+        
         //create pointers to child and parent directory
-        DirectoryTree * dirParent;
-        DirectoryTree * dirChild;
+        DirectoryTree *dirParent;
+        DirectoryTree *dirChild;
     
         DirectoryTree *create_dir(DirectoryTree *node) {
             //code to create node
@@ -56,26 +71,46 @@ class DirectoryTree : File {
 
         DirectoryTree *delete_files(DirectoryTree *node) {
             File fobj;
+            FileData obj;
             //Send each file of this directory to a function -
             //- that deletes induvidual files
-            for (int i = 0; i < files.size(); i++)
+            for(int i = 0; i < obj.size; i++)
                 //assuming the column '0' of dirTables holds pointer to file location on hard disk
-                fobj.delete_file(files[i][0]);
+                fobj.delete_file(obj.file);
         }
 
         DirectoryTree *createFilefromDir(DirectoryTree *node) {
-            //create an object of 'File' (BST type), creates place in memory fot this file 
-            File new_file;
-            //append the location of new_file to FileTable
 
-            //generate index value for file
-            int index;
+            File fobj;
+            fstream stream_obj;
+            string fname;
+            cout << "File name? with proper extension plz\n";
+            cin >> fname;
 
-            //find file size of temporary file;
-            int size;
+            //additional code needs to be added to check if the file already exists in the system.
+            //Hence search functions need to still be implemented
 
-            //call create file function to assign the file values;
-            create_file(index, size, std::addressof(new_file));
+            //Write code to open up the file to a text editor
+            stream_obj.open(fname, ios::out);
+            //rest of this code should execute after the person hits ctrl + save
+
+
+            //find all the file values and assign them to Filedata Structure. 
+            //set filename
+            new_file[total_no_of_files].filename = fname;
+            //set file object
+            new_file[total_no_of_files].file = &fobj;
+            //some method to retrieve file size
+                //new_file.size = fobj.find_size(file);
+            //some method to find file extension ~ magic number
+                //new_file.size = fobj.find_type(file);
+            //some method to set timestamp at time of creation
+
+            //call create file function the file values;
+            fobj.create_file(&fobj, new_file[total_no_of_files].size, &stream_obj);
+
+            //since we have created a new file :
+            total_no_of_files ++;
         }
 
         DirectoryTree *modify_contents(DirectoryTree *node) {
@@ -86,44 +121,50 @@ class DirectoryTree : File {
 
 /*_________________________________________________________________________BST for VERSION CONTROL___________________________________________________________________*/
 
+/*Left to Implement:
+1.Search File
+2.Modify File
+*/
 class File {
     private:
         //file features can be stored as private objects for now
-        string hashValue;
+        //removed hash value for simplicity sake, since our BST sorting is based on file size
+        fstream *stream_obj;
         int size;
-        int index;
     public:
         //Implement version control - with BST
         File *lchild;
         File *rchild;
         File *parent;
         //technically creates nodes to the file BST's
-        File *create_file(int index, int size, File *location) {
+        //location is a node initialized in the directory class and sent here to be inserted into the BST
+        File *create_file(File *location, int size, fstream *stream_obj) {
             if(location == NULL) {
                 //assign private variables their values
-                this->index = index;
+                this->stream_obj = stream_obj;
+                //assign stream object pointer to the stream object of the file class.
+                //So pointer to file location is technically just copied
                 this->size = size;
-                //generate hash value for file being created
-                this->hashValue = generate_hash(location);
             }
 
-            //rchild
+            //rchild (less than)
             else if(location -> size < size) {
-                location -> rchild = create_file(index, size, location -> rchild);
+                location -> rchild = create_file(location -> rchild, size, stream_obj);
                 location -> rchild -> parent = location;
             }
 
-            //lchild
+            //lchild (greater than or equal to)
             else {
-                location -> lchild = create_file(index, size, location -> rchild);
+                location -> lchild = create_file(location -> rchild, size, stream_obj);
                 location -> lchild -> parent = location;
             }
         }
 
         //Clear out file data from BST nodes
-        File *delete_file(string node) {
+        //called from directory class
+        File *delete_file(File *node) {
             //compute file size()
-            int size = node.size();
+            int size;
             //Delete BST node
             if(node == NULL || node->size == size) 
                 return delete_root(node);
@@ -159,8 +200,21 @@ class File {
             x -> lchild = node -> lchild;
             return node -> rchild;
         }
-
-        string generate_hash(File *location) {
-            //Write hash function here
-        }
 };
+
+
+
+
+/*
+Flow of content if user needs to create a File (First File in filesystem):
+
+User gives command to Create Directory:
+Object of type DirectoryTree created -> So root Directory created & vector<Filedata> initialized.
+User must Enter into Directory:
+User must give command to Create File:
+DirectoryTree *createFilefromDir() ->  creates a file with fstream and opens a text editor
+User saves file and exits:
+DirectoryTree *createFilefromDir() -> sets file metaData into vector<Filedata> -> creates a File class object and initializes BST -> calls File *create_file()
+File *create_file() -> saves the address of file into the fstream object of it's class
+
+*/
